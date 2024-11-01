b8524df3ee1aff49a032f8f2c886f7ff445675ac
Ashok S <ashok.s@koenig-solutions.com>
2024-11-01 10:42:14 +0000

    Included h1 tag in all responses

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5100
 app.get('/', (req, res)=>{
-    res.send('This is node app default URL response')
+    res.send('<h1>This is node app default URL response</h1>')
 })
 
 app.get('/about', (req, res)=>{
-    res.send('This is node app about URL response')
+    res.send('<h1>This is node app about URL response</h1>')
 })
 
 app.listen(port, ()=> console.log(`Server is running at http://localhost:${port}`))
