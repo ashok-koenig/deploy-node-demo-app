91389a3334d704a62370ab8d38f2c3f1bf2f4508
Ashok S <ashok.s@koenig-solutions.com>
2024-11-01 10:45:57 +0000

    Users with json data as response

--- app.js
+++ app.js
@@ -9,4 +9,14 @@ app.get('/about', (req, res)=>{
     res.send('<h1>This is node app about URL response</h1>')
 })
 
+app.get('/users', (req, res)=>{
+    let users = [
+        {id: 1, username: "user1", role: "user"},
+        {id: 2, username: "user2", role: "user"},
+        {id: 3, username: "admin1", role: "admin"}
+    ]
+    res.json(users)
+})
+
+
 app.listen(port, ()=> console.log(`Server is running at http://localhost:${port}`))
