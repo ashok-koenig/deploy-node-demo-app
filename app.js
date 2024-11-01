97416d30da6e9d4bced78869a9c67f0748167173
Ashok S <ashok.s@koenig-solutions.com>
2024-11-01 10:57:54 +0000

    Testing ci cd

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.get('/products', (req, res)=>{
     let products = [
         {id: 1, title: "iPhone 16", price: 12312},
         {id: 2, title: "iPhone 15", price: 1233},
-        {id: 3, title: "iPhone 14", price: 1231}
+        {id: 3, title: "iPhone 14", price: 1231},
+        {id: 4, title: "iPhone 13", price: 1230}
     ]
     res.json(products)
 })
